aedb5e6df53e97ce2e46b88681079551376f17d6
rusty-gr <apple9872@googlemail.com>
2012-03-27 01:59:30 +0200

    marked the aggregator as deprecated, due to necessary changes of this interface.

--- shardlet/src/main/java/com/gamerevision/rusty/realityshard/shardlet/EventAggregator.java
+++ shardlet/src/main/java/com/gamerevision/rusty/realityshard/shardlet/EventAggregator.java
@@ -11,12 +11,15 @@ package com.gamerevision.rusty.realityshard.shardlet;
  * An EventAggregator is the EventAggregator-pattern class implemented by the container
  * that will manage the events coming from or triggering the shardlets.
  * 
+ * TODO: After the listener updates, this interface needs to be fixed!
+ * 
  * @author _rusty
  */
 public interface EventAggregator
 {
     
     /**
+     * @deprecated TODO: fix this shit
      * Register a new listener with the event->listeners associations.
      * 
      * @param       event                   The listener will be bound to this event. Whenever
@@ -24,10 +27,11 @@ public interface EventAggregator
      *                                      with a concrete implementation of this event.
      * @param       listener                The listener that will be triggered by the event
      */
-    public void addListener(ShardletEvent event, ShardletEventListener listener);
+    public void addListener(ShardletEvent event, Object listener);
     
     
     /**
+     * @deprecated TODO: fix this shit
      * Trigger an event, the EventAggregator will try to distribute it to all 
      * registered listeners
      * 
